aba89b4e2a917398ae258bf66e1d002a43f6b6b3
Lasith Dissanayake <86358728+DissanayakeLYB@users.noreply.github.com>
2024-03-25 01:00:24 +0530

    finals - 25/03/2024

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-// JavaScript to toggle header visibility on scroll
-window.onscroll = function() {
+window.addEventListener('scroll', function() {
     var header = document.querySelector('header');
     if (window.pageYOffset > document.querySelector('.hero').offsetHeight) {
+        header.style.transition = 'transform 0.3s ease';
         header.style.transform = 'translateY(0)';
     } else {
+        header.style.transition = 'transform 0.3s ease';
         header.style.transform = 'translateY(-100%)';
     }
-}
+});
