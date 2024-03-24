7e53ec6221a4687641d9376717599ddedf427334
Lasith Dissanayake <86358728+DissanayakeLYB@users.noreply.github.com>
2024-03-25 00:20:34 +0530

    final

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-// JavaScript code can be added here for dynamic functionality
+// JavaScript to toggle header visibility on scroll
+window.onscroll = function() {
+    var header = document.querySelector('header');
+    if (window.pageYOffset > document.querySelector('.hero').offsetHeight) {
+        header.style.transform = 'translateY(0)';
+    } else {
+        header.style.transform = 'translateY(-100%)';
+    }
+}
